b4372f2d58e97642f77f2d66da146709a3cfb9c2
unknown <tobiburrohman2@gmail.com>
2022-10-01 08:31:56 +0600

    e.nv and mongo is connected

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middleware
@@ -14,6 +15,25 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', async(req, res)=>{
     res.send("Heello WORLD")
 });
+
+// user: creative-agency
+// pswd: kZcPFMy7eaFZDkX9
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    console.log('mongo connected')
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+// app.get('/services', async(req, res)=>{
+//     const result = await serviceCollection.find({}).toArray();
+//     res.send(result)
+// })
 
 
 app.listen(port, (req, res)=>{
